7b7c9c633d994040454662fb6ef1ba8a778a5bde
Christoph Wille <christoph.wille@gmail.com>
2023-05-02 09:21:30 +0200

    Update to 0.5.0 (see https://github.com/extism/extism/issues/325)

--- src/ExtismHello/Program.cs
+++ src/ExtismHello/Program.cs
@@ -1,9 +1,10 @@
-﻿using Extism.Sdk.Native;
+﻿using Extism.Sdk;
+using Extism.Sdk.Native;
 using System.Text;
 
 var context = new Context();
 var wasm = await File.ReadAllBytesAsync("code.wasm");
-using var plugin = context.CreatePlugin(wasm, withWasi: true);
+using var plugin = context.CreatePlugin(wasm, new HostFunction[0], withWasi: true);
 
 var output = Encoding.UTF8.GetString(
     plugin.CallFunction("count_vowels", Encoding.UTF8.GetBytes("Hello World!"))
